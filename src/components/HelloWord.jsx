60e6d0cf71f9488cf5dab5883e5c240dd68b7536
hector signoriello <hector.signoriello@gmail.cm>
2019-09-07 23:57:19 -0300

    finish configuration initial

--- src/components/HelloWord.jsx
+++ src/components/HelloWord.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "../assets/styles/App.scss";
+import React from 'react';
+import '../assets/styles/App.scss';
 
 const HelloWord = () => <h1>Hola Mundo</h1>;
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import HelloWord from "./components/HelloWord";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWord from './components/HelloWord';
 
-ReactDOM.render(<HelloWord />, document.getElementById("app"));
+ReactDOM.render(<HelloWord />, document.getElementById('app'));
