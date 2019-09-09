3b8cdd05844118cfe4beff57f77fcf57b69f2dc5
hector signoriello <hector.signoriello@gmail.cm>
2019-09-09 00:08:49 -0300

    hooks API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,31 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 // styles
 import '../assets/styles/App.scss';
 
-const App = () => {
-  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+const API = 'http://localhost:3000/initialState';
 
-  useEffect(() => {
-    fetch('http://localhost:3000/initalState')
-      .then((response) => response.json())
-      .then((data) => setVideos(data));
-  });
+const App = () => {
+  const initialState = useInitialState(API);
 
   return (
     <div className='App'>
       <Header />
       <Search />
-      {videos.mylist.length > 0 && (
+      {initialState.mylist.length > 0 && (
         <Categories title='Mi Lista'>
           <Carousel>
-            {videos.mylist.map((item) => (
+            {initialState.mylist.map((item) => (
               <CarouselItem key={item.id} {...item} />
             ))}
           </Carousel>
@@ -33,14 +30,14 @@ const App = () => {
       )}
       <Categories title='Tendencias'>
         <Carousel>
-          {videos.trends.map((item) => (
+          {initialState.trends.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
       </Categories>
       <Categories title='Originales de PlatziVideos'>
         <Carousel>
-          {videos.originals.map((item) => (
+          {initialState.originals.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
