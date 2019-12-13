3fe038a11005f42d0115be5bc2078acf26112d14
Hector Signoriello <hector.signoriello@gmail.com>
2019-12-13 01:27:10 -0300

    add and config redux Store, and initialState hardcoding

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,25 +1,24 @@
+//Import Librarys
 import React from 'react';
+import { connect } from 'react-redux';
+// Import Components
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
 
 // styles
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initialState';
-
-const Home = () => {
-  const initialState = useInitialState(API);
+const Home = ({ myList, trends, originals }) => {
 
   return (
     <>
       <Search />
-      {initialState.mylist.length > 0 && (
+      {myList.length > 0 && (
         <Categories title='Mi Lista'>
           <Carousel>
-            {initialState.mylist.map((item) => (
+            {myList.map((item) => (
               <CarouselItem key={item.id} {...item} />
             ))}
           </Carousel>
@@ -27,14 +26,14 @@ const Home = () => {
       )}
       <Categories title='Tendencias'>
         <Carousel>
-          {initialState.trends.map((item) => (
+          {trends.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
       </Categories>
       <Categories title='Originales de PlatziVideos'>
         <Carousel>
-          {initialState.originals.map((item) => (
+          {originals.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
@@ -43,4 +42,12 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+// export default connect(props, actions)(Home);
+export default connect(mapStateToProps, null)(Home);
