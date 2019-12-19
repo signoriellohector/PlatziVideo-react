b836fd0fe4a0f64fa945dca936a9f7050f0131bf
Hector Signoriello <hector.signoriello@gmail.com>
2019-12-19 01:42:10 -0300

    session validation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 
 // styles
 import '../assets/styles/components/Header.scss';
@@ -15,6 +17,10 @@ const Header = (props) => {
 
   const hasUser = Object.keys(user).length > 0;
 
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+
   return (
     <header className='header'>
       {/* Go back to Home */}
@@ -24,20 +30,47 @@ const Header = (props) => {
 
       <div className='header__menu'>
         <div className='header__menu--profile'>
+
+          {/* validando login para gravatar */}
           {hasUser ? (
             <img src={gravatar(user.email)} alt={user.email} />
           ) : (
             <img src={userIcon} alt='' />
           )}
+
           <p>Perfil</p>
         </div>
         <ul>
-          <li><Link to='/'>Cuenta</Link></li>
-          <li><Link to='/login'>Iniciar Sesión</Link></li>
+
+          {/* validando login */}
+          {hasUser ? (
+            <li><Link to='/'>{user.name}</Link></li>
+          ) : (
+            null
+          )}
+
+          {/* validando logout */}
+          {hasUser ? (
+            <li>
+              <a href='#logout' onClick={handleLogout}>Cerrar Sesíon</a>
+            </li>
+          ) : (
+            <li>
+              <Link to='/login'>
+              Iniciar Sesión
+              </Link>
+            </li>
+          )}
+
         </ul>
       </div>
     </header>
   );
+};
+
+header.propTypes = {
+  user: propTypes.object.isRequired,
+  logoutRequest: propTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => {
@@ -45,4 +78,9 @@ const mapStateToProps = (state) => {
     user: state.user,
   };
 };
-export default connect(mapStateToProps, null)(Header);
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
